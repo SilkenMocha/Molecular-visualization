7a77109dc949f305c701b68a9946b992a16b1fcf
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-04-19 18:34:53 -0600

    update

--- mol_vis_test.py
+++ mol_vis_test.py
@@ -5,10 +5,7 @@ import numpy as np
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
-
-st.title('RDKit + Py3DMOL 😀')
-st.write('Hello world')
-
+st.title('VISUALIZACIÓN MOLECUALR')
 
 def makeblock(smi):
     mol = Chem.MolFromSmiles(smi)
@@ -18,7 +15,7 @@ def makeblock(smi):
     return mblock
 
 def render_mol(xyz):
-    xyzview = py3Dmol.view()(width=1000,height=1000)
+    xyzview = py3Dmol.view()#(width=1000,height=1000)
     xyzview.addModel(xyz,'mol')
     xyzview.setStyle({'stick':{}})
     xyzview.setBackgroundColor('white')
