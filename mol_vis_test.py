1e4dcdb59ee200ccb69989f08a571682cf4d5db2
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-04-24 20:23:19 -0600

    asdfsdfsadf

--- mol_vis_test.py
+++ mol_vis_test.py
@@ -26,21 +26,3 @@ compound_smiles=st.text_input('SMILES please','CC')
 blk=makeblock(compound_smiles)
 render_mol(blk)
 
-import py3Dmol
-from psikit import Psikit
-from rdkit import Chem
-pk = Psikit()
-pk.read_from_smiles('O')
-pk.energy()
-pk.create_cube_files(gridspace=0.5)
-
-homo_voldata = open("Psi_a_5_1-A\"_HOMO.cube", "r").read()
-lumo_voldata = open("Psi_a_6_5-A\'_LUMO.cube", "r").read()
-
-v = py3Dmol.view()
-v.addVolumetricData(homo_voldata, "cube", {'isoval': -0.03, 'color': "red", 'opacity': 0.75})
-v.addVolumetricData(homo_voldata, "cube", {'isoval': 0.03, 'color': "blue", 'opacity': 0.75})
-v.addModel(Chem.MolToMolBlock(pk.mol), 'mol')
-v.setStyle({'stick':{}})
-v.zoomTo()
-v.show()
