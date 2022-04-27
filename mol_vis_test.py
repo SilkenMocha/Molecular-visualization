b80f7df318790e5a12a5267d1b668901a46efa9b
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-04-27 06:34:31 -0600

    sdf

--- mol_vis_test.py
+++ mol_vis_test.py
@@ -17,7 +17,9 @@ if seleccion_molecula == "Subir un archivo":
     #render xyz
     def render_mol(xyz):
        xyzview = py3Dmol.view(width=400,height=400)
+       xyzview.addModel(xyz,'xyz')
        xyzview.addModel(xyz,'sdf')
+       #xyzview.addModel(xyz,'mol')
        xyzview.setStyle({'stick':{}})
        xyzview.setBackgroundColor('white')#('0xeeeeee')
        xyzview.zoomTo()
