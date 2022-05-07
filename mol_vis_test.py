a00ccf240807abe03012ebe34c3e28fafe58a481
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-05-06 20:19:34 -0600

    sdf

--- mol_vis_test.py
+++ mol_vis_test.py
@@ -153,6 +153,15 @@ if seleccion == "Visualizacion molecular":
       st.pyplot(fig2)
 
       st.subheader("Pharmacophore")
+      txt = st.text_area('Text to analyze', '''
+     It was the best of times, it was the worst of times, it was
+     the age of wisdom, it was the age of foolishness, it was
+     the epoch of belief, it was the epoch of incredulity, it
+     was the season of Light, it was the season of Darkness, it
+     was the spring of hope, it was the winter of despair, (...)
+     ''')
+      st.write('Sentiment:', run_sentiment_analysis(txt))
+      
       with st.expander("Abreviaciones"):
         st.write("HA: Hydrogen-bond acceptor")
         st.write("HD: Hydrogen-bond donor")
